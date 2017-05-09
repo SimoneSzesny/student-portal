218a1fa96294c1e998cbf9a98c576bf9497bf712
Simone <simoneszesny@gmail.com>
2017-05-08 19:04:33 -0500

    Initail commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,5 @@
+class UserController < ApplicationController
+  def index
+    render "index.html.erb"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get "/student_portal" => "user#index"
+end
