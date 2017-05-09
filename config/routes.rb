369b45852e0621b2faa4257646b6f7d4d380b0d9
Simone <simoneszesny@gmail.com>
2017-05-08 20:56:29 -0500

    login created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get "/student_portal" => "user#index"
-  get "/students/new" => "student#new"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
 end
