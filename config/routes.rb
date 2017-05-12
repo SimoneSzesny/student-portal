5f0dfd741958fdb795283c0a4f0cb26090ce3732
James Hughes <james.a.hughes@gmail.com>
2017-05-11 19:17:48 -0500

    Add routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,13 @@ Rails.application.routes.draw do
   get "/students/show" => "student#show"
 
   get "/experiences/:id/edit" => "experiences#edit"
+  patch "/experiences/:id" => "experiences#update"
   get "/capstones/:id/edit" => "capstones#edit"
+  patch "/capstones/:id" => "capstones#update"
   get "/skills/:id/edit" => "skills#edit"
+  patch "/skills/:id" => "skills#update"
   get "/educations/:id/edit" => "educations#edit"
+  patch "/educations/:id" => "educations#update"
 
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
